8ded3d2d5a9da709b6b1085397f4b52eb5c116f1
Sabarinathan Gopal <gopal.sabarinathan@ni.com>
2024-09-30 17:56:21 +0530

    fix: revamp the folder structure.

---
+++ src/csv_logger/stubs/__init__.py
@@ -0,0 +1,5 @@
+
+from stubs.log_measurement_pb2 import LogMeasurementResponse
+from stubs.log_measurement_pb2_grpc import LogMeasurementServicer, add_LogMeasurementServicer_to_server
+
+__all__ = ["LogMeasurementResponse", "LogMeasurementServicer", "add_LogMeasurementServicer_to_server"]
